de1fd00e926359a276e4c6927a3e085c80e12ec3
zhoushoujian <863165094@qq.com>
2020-09-06 12:59:16 +0800

    multi-svgs-viewer

--- plugin/js/main.tsx
+++ plugin/js/main.tsx
@@ -14,18 +14,18 @@ function MyDropzone() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div className="show-svg-container">
+    <div className="svg-viewer-chrome-plugin-css-style-show-svg-container">
       {
         !svgFiles.length
-          ? <div title={`${isDragActive ? 'Drop the files here ...' : 'Drag some files here, or click to select files'}`} className="multi-svg-container">
+          ? <div title={`${isDragActive ? 'Drop the files here ...' : 'Drag some files here, or click to select files'}`} className="svg-viewer-chrome-plugin-css-style-multi-svg-container">
             <div {...getRootProps({ accept: "image/svg+xml" })}>
               <input {...getInputProps()} />
-              <div className="plugin-header-init">Multiple SVGS Viewer</div>
+              <div className="svg-viewer-chrome-plugin-css-style-plugin-header-init">Multiple SVGS Viewer</div>
               {
                 isDragActive ?
-                  <div className="upload-button-init">Drop the files here ...</div> :
-                  <div className="upload-button-init">
-                    <div className="upload-button-init-middle">
+                  <div className="svg-viewer-chrome-plugin-css-style-upload-button-init">Drop the files here ...</div> :
+                  <div className="svg-viewer-chrome-plugin-css-style-upload-button-init">
+                    <div className="svg-viewer-chrome-plugin-css-style-upload-button-init-middle">
                       <div style={{ fontSize: '72px' }}>+</div>
                       <div style={{ textAlign: 'center' }}>Drag some files here, or click to select files</div>
                     </div>
@@ -34,26 +34,26 @@ function MyDropzone() {
             </div>
           </div>
           : <Fragment>
-            <h2 className="plugin-header-content">Multiple SVGS Viewer</h2>
-            <div style={{ width: "100%" }} title={`${isDragActive ? 'Drop the files here ...' : 'Drag some files here, or click to select files'}`} className="multi-svg-container">
-              <div className="svg-items-container">
+            <h2 className="svg-viewer-chrome-plugin-css-style-plugin-header-content">Multiple SVGS Viewer</h2>
+            <div style={{ width: "100%" }} title={`${isDragActive ? 'Drop the files here ...' : 'Drag some files here, or click to select files'}`} className="svg-viewer-chrome-plugin-css-style-multi-svg-container">
+              <div className="svg-viewer-chrome-plugin-css-style-svg-items-container">
                 {
                   svgFiles.map((file, index) => (
-                    <div className="svg-items" key={index}>
-                      <div className="filename" title={file.name}>{file.name}</div>
-                      <img alt="" className="react-svg" src={window.webkitURL.createObjectURL(file)} />
+                    <div className="svg-viewer-chrome-plugin-css-style-svg-items" key={index}>
+                      <div className="svg-viewer-chrome-plugin-css-style-filename" title={file.name}>{file.name}</div>
+                      <img alt="" className="svg-viewer-chrome-plugin-css-style-react-svg" src={window.webkitURL.createObjectURL(file)} />
                     </div>
                   ))
                 }
-                <div className="svg-items upload-button-content">
+                <div className="svg-viewer-chrome-plugin-css-style-svg-items svg-viewer-chrome-plugin-css-style-upload-button-content">
                   <div {...getRootProps()}>
                     <input {...getInputProps()} />
                     {
                       isDragActive ?
-                        <div className="upload-button-content-border drop-file-here">Drop the files here ...</div> :
-                        <div className="upload-button-content-border">
-                          <div className="plus">+</div>
-                          <div className="plus-text">Drag some files here, or click to select files</div>
+                        <div className="svg-viewer-chrome-plugin-css-style-upload-button-content-border svg-viewer-chrome-plugin-css-style-drop-file-here">Drop the files here ...</div> :
+                        <div className="svg-viewer-chrome-plugin-css-style-upload-button-content-border">
+                          <div className="svg-viewer-chrome-plugin-css-style-plus">+</div>
+                          <div className="svg-viewer-chrome-plugin-css-style-plus-text">Drag some files here, or click to select files</div>
                         </div>
                     }
                   </div>
