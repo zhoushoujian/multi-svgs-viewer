1b58a71459ad5f58d6d3d165dde67d14bc3af827
zhoushoujian <863165094@qq.com>
2021-06-27 23:01:56 +0800

    feat: optimize experience

--- plugin/js/index.tsx
+++ plugin/js/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
 
-const generateForHtml = true;
+const generateForHtml = false;
 
 window.onload = () => {
   if (
