c28dfdd5bc4b7f1f7190678fe818940a1af973d6
zhoushoujian <863165094@qq.com>
2021-06-27 21:23:39 +0800

    optimize experience

--- plugin/js/main.tsx
+++ plugin/js/main.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable indent */
 /* eslint-disable @typescript-eslint/indent */
 /* eslint-disable no-nested-ternary */
-import React, { useCallback, useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { toast, ToastContainer } from 'react-toastify';
 import VirtualizedMasonry from './masonry';
@@ -60,10 +60,12 @@ const selectedBtns = {
 
 let mimeObjectInfo = {};
 const picWidth = 200;
+const initState = 'Drag some files here, or click to select files';
 
 function MyDropzone() {
   const headerRef = useRef(null as any);
   const [availableFiles, setAvailableFiles] = useState<any[]>([]);
+  const [uploadState, setUploadState] = useState(initState);
 
   useEffect(() => {
     console.log('multi-svgs-viewer active');
@@ -72,55 +74,60 @@ function MyDropzone() {
     }
   }, []);
 
-  const onDrop = useCallback(
-    acceptedFiles => {
-      console.log('selected files: ', acceptedFiles);
-      acceptedFiles = acceptedFiles.filter(item => {
-        const mimeInfo = mimeObjectInfo[item.type];
-        mimeObjectInfo[item.type] = mimeInfo ? mimeInfo + 1 : 1;
-        return availableMimeArray.indexOf(item.type) !== -1;
-      });
-      console.log('mime count', mimeObjectInfo);
-      mimeObjectInfo = {};
-      acceptedFiles.forEach(item => {
-        if (item.type === 'image/svg+xml') {
-          item.visible = selectedBtns.svgBtn ? true : false;
-        } else if (item.type === 'image/gif') {
-          item.visible = selectedBtns.gifBtn ? true : false;
-        } else if (item.type === 'image/jpeg') {
-          item.visible = selectedBtns.jpgBtn ? true : false;
-        } else if (item.type === 'image/bmp') {
-          item.visible = selectedBtns.bmpBtn ? true : false;
-        } else if (item.type === 'image/x-icon') {
-          item.visible = selectedBtns.iconBtn ? true : false;
-        } else if (item.type === 'image/png') {
-          item.visible = selectedBtns.pngBtn ? true : false;
-        }
-      });
-      console.log('available files: ', acceptedFiles);
-      return dealAvailableFiles([...availableFiles, ...acceptedFiles]);
-    },
-    [availableFiles]
-  );
-
-  const dealAvailableFiles = files => {
-    const length = files.length;
-    toast.info(
+  const onDrop = acceptedFiles => {
+    toast.info('got files', {
+      position: 'top-right',
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined
+    });
+    console.log('selected files: ', acceptedFiles);
+    acceptedFiles = acceptedFiles.filter(item => {
+      const mimeInfo = mimeObjectInfo[item.type];
+      mimeObjectInfo[item.type] = mimeInfo ? mimeInfo + 1 : 1;
+      return availableMimeArray.indexOf(item.type) !== -1;
+    });
+    console.log('mime count', mimeObjectInfo);
+    const length = acceptedFiles.length;
+    const info =
       length > 100
         ? `Loading ${length} files, it maybe cost much time, please wait for patient...`
-        : `Loading ${length} files, please wait...`,
-      {
-        position: 'top-right',
-        autoClose: length > 100 ? 10000 : 3000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined
+        : `Loading ${length} files, please wait...`;
+    toast.info(info, {
+      position: 'top-right',
+      autoClose: length > 100 ? 10000 : 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined
+    });
+    mimeObjectInfo = {};
+    acceptedFiles.forEach(item => {
+      if (item.type === 'image/svg+xml') {
+        item.visible = selectedBtns.svgBtn ? true : false;
+      } else if (item.type === 'image/gif') {
+        item.visible = selectedBtns.gifBtn ? true : false;
+      } else if (item.type === 'image/jpeg') {
+        item.visible = selectedBtns.jpgBtn ? true : false;
+      } else if (item.type === 'image/bmp') {
+        item.visible = selectedBtns.bmpBtn ? true : false;
+      } else if (item.type === 'image/x-icon') {
+        item.visible = selectedBtns.iconBtn ? true : false;
+      } else if (item.type === 'image/png') {
+        item.visible = selectedBtns.pngBtn ? true : false;
       }
-    );
+    });
+    console.log('available files: ', acceptedFiles);
+    return dealAvailableFiles([...availableFiles, ...acceptedFiles]);
+  };
+
+  const dealAvailableFiles = files => {
     return Promise.all(
-      files.map(file => {
+      files.map((file, index) => {
         return new Promise(res => {
           const reader = new FileReader();
           reader.onload = function (e: any) {
@@ -130,6 +137,14 @@ function MyDropzone() {
             image.onload = function () {
               const height = image.height * (picWidth / image.width);
               Object.assign(file, { width: picWidth, height });
+              image.width = picWidth;
+              image.height = height;
+              if (file.size > 1 * 1024 * 1024) {
+                const src = compress(image, 0.4, file.type);
+                Object.assign(file, { src });
+                // console.info(`正在加载第${index + 1}张图片 - ${file.name}...`);
+                setUploadState(`Loading ${index + 1} pictures - ${file.path}...`);
+              }
               res(null);
             };
             image.src = data;
@@ -142,12 +157,23 @@ function MyDropzone() {
       })
     )
       .then(() => {
+        setUploadState(initState);
         setAvailableFiles(files);
       })
       .catch(err => {
         console.error('Load error err', err);
         alert('Load error');
       });
+  };
+
+  const compress = (image, quality, type) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context?.drawImage(image, 0, 0, canvas.width, canvas.height);
+    const res = canvas.toDataURL(type, quality);
+    return res;
   };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -169,10 +195,7 @@ function MyDropzone() {
               </div>
             ) : (
               <div className="svg-viewer-chrome-plugin-css-style-upload-button-content-border">
-                <div className="svg-viewer-chrome-plugin-css-style-plus">+</div>
-                <div className="svg-viewer-chrome-plugin-css-style-plus-text">
-                  Drag some files here, or click to select files
-                </div>
+                <div className="svg-viewer-chrome-plugin-css-style-plus-text">{uploadState}</div>
               </div>
             )}
           </div>
@@ -220,7 +243,7 @@ function MyDropzone() {
                     title={file.path}
                     alt={file.name}
                     className="svg-viewer-chrome-plugin-css-style-react-svg"
-                    src={window.webkitURL.createObjectURL(file)}
+                    src={file.src || window.webkitURL.createObjectURL(file)}
                   />
                 </div>
               );
